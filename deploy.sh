05c54904d64080ae8f666d76d33a650de47c4479
wanwusangzhi1992 <wanwusangzhi1992@163.com>
2018-12-08 15:36:57 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+echo '\n=== checking status'
 git status
 
+echo '\n=== add files'
 git add ./
 
+echo '\n=== add commit remark'
 git commit -m 'update'
 
+echo '\n=== git pull'
 git pull
 
+echo '\n=== git push'
 git push
 
-echo 'done'
+echo '\n=== done'
